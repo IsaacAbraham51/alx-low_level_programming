28fe598c7ebd2c2d579a8be5a024e72e8820d0b4
isaac <106970737+IsaacAbraham51@users.noreply.github.com>
2022-07-28 20:41:14 -0300

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,7 +3,13 @@
 
 #include <stdio.h>
 
+void free_grid(int **grid, int height);
 void *malloc_checked(unsigned int b);
+char *_strdup(char *str);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+int **aloc_grid(int width, int height);
+char *argstostr(int ac, char **av);
 
 #endif /* MAIN_H */
