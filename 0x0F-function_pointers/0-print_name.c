8c20fd358657d29e8fdf6be55f6cbf2f28822c6c
isaac <106970737+IsaacAbraham51@users.noreply.github.com>
2022-08-03 16:42:02 -0300

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include<stddef.h>
+/**
+ * print_name - a function ...
+ * @name: the chaine
+ * @f: the function
+ *
+ * Return: 1 or 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+	(*f)(name);
+}
+
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Ter
