82fbed0fc16b4efc625af3efed105a121fb6fbb4
isaac <106970737+IsaacAbraham51@users.noreply.github.com>
2022-08-22 23:45:09 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,39 @@
 #include <stdio.h>
-#include "main.h"
+#include "holberton.h"
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
-
-#define BUF_SIZE 9096
-
+#include <stdlib.h>
 /**
- * create_file - a function ...
- * @filename: the list
- * @text_content: the number
- *
- * Return: 1 or 0
+ * create_file - Creates a file.
+ * standard output.
+ * @filename: Name of the file to create.
+ * @text_content: NULL terminated string to write to the file.
+ * Return: 1 on success, -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, lenght;
+	ssize_t res_write;
 
 	if (filename == NULL)
 		return (-1);
-
-	fd = open(filename, O_CREAT | O_RDWR, 600);
-
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-
-	chmod(filename, 644);
-
 	if (text_content != NULL)
-		write(fd, text_content, strlen(text_content));
+	{
+		lenght = 0;
+		while (*(text_content + lenght) != '\0')
+			lenght++;
+		res_write = write(fd, text_content, lenght);
+		if (res_write == -1)
+		{
+			write(1, "fails", 6);
+			return (-1);
+		}
+	}
 	close(fd);
 	return (1);
 }
